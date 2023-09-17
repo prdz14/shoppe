20a4b632c2376726aa31c5804c0c3f699fbec764
amin <mihakh13@mail.ru>
2023-09-17 20:39:33 +0300

    third

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/shoppe',
   plugins: [vue()],
-  resolve:{
-    alias:{
+  resolve: {
+    alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
